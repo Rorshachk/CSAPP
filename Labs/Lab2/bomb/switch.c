10c4c6589c0eda7aca60e2984d729897c5552bdd
Rorshachk <shuangmuhu@gmail.com>
2019-10-28 05:59:27 +0800

    update bomb lab

---
+++ Labs/Lab2/bomb/switch.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int x; x = 3;
+    int y;
+    switch(x){
+        case 1: y = 1; break;
+        case 2: y = 2; break;
+    }
+    return 0;
+}
---
+++ Labs/Lab2/bomb/switch.s
@@ -0,0 +1,14 @@
+	.file	"switch.c"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB23:
+	.cfi_startproc
+	movl	$0, %eax
+	ret
+	.cfi_endproc
+.LFE23:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.11) 5.4.0 20160609"
+	.section	.note.GNU-stack,"",@progbits
