fa9fd6befc6ba522f7616377f2c6a0041024026e
root <root@vultr.guest>
2019-10-31 07:09:19 +0000

    update attack lab

---
+++ Labs/Lab3/target1/nop.asm
@@ -0,0 +1,8 @@
+
+nop.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	90                   	nop
---
+++ Labs/Lab3/target1/test.d
@@ -0,0 +1,11 @@
+
+test.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	68 ef cd ab 00       	pushq  $0xabcdef
+   5:	48 83 04 25 00 00 00 	addq   $0x11,0x0
+   c:	00 11 
+   e:	89 c2                	mov    %eax,%edx
