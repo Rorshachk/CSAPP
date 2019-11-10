42512fc0b1b8b767ac0404db1a757f6889f509d4
Rorshachk <shuangmuhu@gmail.com>
2019-11-11 03:15:17 +0800

    update cache lab

--- Labs/Lab4/cachelab-handout/trans.c
+++ Labs/Lab4/cachelab-handout/trans.c
@@ -22,14 +22,99 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
-    int i, j, tmp;
+    if (M == 32 && N == 32) {
+    //    int t[8];
+        int t1, t2, t3, t4, t5, t6, t7, t8;
+        for (int i = 0; i < N; i += 8)
+          for (int j = 0; j < M; j += 8)
+            for(int l = i; l < i + 8; l++){
+                for(int k = j; k < j + 8; k++){  //不能用数组= =
+                    t1 = A[k][l];
+                    t2 = A[k][l + 1];
+                    t3 = A[k][l + 2];
+                    t4 = A[k][l + 3];
+                    t5 = A[k][l + 4];
+                    t6 = A[k][l + 5];
+                    t7 = A[k][l + 6];
+                    t8 = A[k][l + 7];
+                    B[l][k] = t1;
+                    B[l + 1][k] = t2;
+                    B[l + 2][k] = t3;
+                    B[l + 3][k] = t4;
+                    B[l + 4][k] = t5;
+                    B[l + 5][k] = t6;
+                    B[l + 6][k] = t7;
+                    B[l + 7][k] = t8;
+                }
+    //              t[k - j] = A[l][k];
+    //            for(int k = j; k < j + 8; k++)
+    //              B[k][l] = t[k - j];
+            }
+    }
+    if(M == 64 && N == 64){
+        int t1, t2, t3, t4, t5, t6, t7, t8;
+        for(int i = 0; i < N; i += 8)
+          for(int j = 0; j < M; j += 8){
+              for(int k = i; k < i + 4; k++){
+                  t1 = A[k][j];
+                  t2 = A[k][j + 1];
+                  t3 = A[k][j + 2];
+                  t4 = A[k][j + 3];
+                  t5 = A[k][j + 4];
+                  t6 = A[k][j + 5];
+                  t7 = A[k][j + 6];
+                  t8 = A[k][j + 7];
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
-            tmp = A[i][j];
-            B[j][i] = tmp;
-        }
-    }    
+                  B[j][k] = t1;
+                  B[j + 1][k] = t2;
+                  B[j + 2][k] = t3;
+                  B[j + 3][k] = t4;
+                  B[j][k + 4] = t5;
+                  B[j + 1][k + 4] = t6;
+                  B[j + 2][k + 4] = t7;
+                  B[j + 3][k + 4] = t8;
+              }
+              for(int k = j; k < j + 4; k++){
+                  t1 = A[i + 4][k];
+                  t2 = A[i + 5][k];
+                  t3 = A[i + 6][k];
+                  t4 = A[i + 7][k];
+                  t5 = B[k][i + 4];
+                  t6 = B[k][i + 5];
+                  t7 = B[k][i + 6];
+                  t8 = B[k][i + 7];
+
+                  
+                  B[k][i + 4] = t1;
+                  B[k][i + 5] = t2;
+                  B[k][i + 6] = t3;
+                  B[k][i + 7] = t4;
+
+                  B[k + 4][i] = t5;
+                  B[k + 4][i + 1] = t6;
+                  B[k + 4][i + 2] = t7;
+                  B[k + 4][i + 3] = t8;
+
+                  t1 = A[i + 4][k + 4];
+                  t2 = A[i + 5][k + 4];
+                  t3 = A[i + 6][k + 4];
+                  t4 = A[i + 7][k + 4];
+
+                  B[k + 4][i + 4] = t1;
+                  B[k + 4][i + 5] = t2;
+                  B[k + 4][i + 6] = t3;
+                  B[k + 4][i + 7] = t4;
+              }
+          }
+    }
+    if(M == 61 && N == 67){
+        int siz = 23;
+        for(int i = 0; i < N; i += siz)
+          for(int j = 0; j < M; j += siz)
+            for(int k = i; k < i + siz && k < N; k++)
+              for(int t = j; t < j + siz && t < M; t++)
+                B[t][k] = A[k][t];
+    }
 }
 
 /* 
