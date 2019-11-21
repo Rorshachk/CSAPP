d0de1027270de7e7dfb161bbb711765adff4a6cf
Rorshachk <shuangmuhu@gmail.com>
2019-11-21 12:14:47 +0800

    update chapter-8

---
+++ Chapter-8/fork.c
@@ -0,0 +1,18 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    pid_t pid;
+    int x = 1;
+
+    pid = Fork();
+    if(pid == 0){
+        printf("child : x = %d\n", ++x);
+        exit(0);
+    }
+
+    printf("parent: x=%d\n", --x);
+    exit(0);
+}
